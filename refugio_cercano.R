962fcf62e263e80b764d0c9a0c51d16fb5b495ad
pautrejo <paulinahtrejo@gmail.com>
2021-10-12 19:51:48 -0500

    funcion municipio

--- refugio_cercano.R
+++ refugio_cercano.R
@@ -4,26 +4,27 @@ library(leaflet)
 
 r2 <- tail(refugios,3)
 
-refugio_cercano <- function (long,lat){
-  #limpieza (diagonal final)
-  r2$coordN <- substr(r2$coordN,1,nchar(r2$coordN)-1)
-  r2$coordW <- substr(r2$coordW,1,nchar(r2$coordW)-1)
-  
-  #texto a coordenadas
-  r2 <- r2 %>% mutate(
-                w = as.double(map(strsplit(r2$coordW,"([º,'])"),1)) + #grados
-                  as.double(map(strsplit(r2$coordW,"([º,'])"),2))/60 + #minutos
-                  round(as.double(map(strsplit(r2$coordW,"([º,'])"),3)))/3600, #segundos
-                n = as.double(map(strsplit(r2$coordN,"([º,'])"),1)) + 
-                  as.double(map(strsplit(r2$coordN,"([º,'])"),2))/60 + 
-                  round(as.double(map(strsplit(r2$coordN,"([º,'])"),3)))/3600)
+#limpieza (diagonal final)
+r2$coordN <- substr(r2$coordN,1,nchar(r2$coordN)-1)
+r2$coordW <- substr(r2$coordW,1,nchar(r2$coordW)-1)
 
-  r2 <- r2 %>% mutate(distancia = distVincentyEllipsoid(c(long,lat),  r2[,17:18])/1000) #en km
+r2 <- r2 %>% mutate(
+  w = as.double(map(strsplit(r2$coordW,"([º,'])"),1)) + #grados
+    as.double(map(strsplit(r2$coordW,"([º,'])"),2))/60 + #minutos
+    round(as.double(map(strsplit(r2$coordW,"([º,'])"),3)))/3600, #segundos
+  n = as.double(map(strsplit(r2$coordN,"([º,'])"),1)) + 
+    as.double(map(strsplit(r2$coordN,"([º,'])"),2))/60 + 
+    round(as.double(map(strsplit(r2$coordN,"([º,'])"),3)))/3600)
+
+
+
+#####con coordenadas#####
+refugio_cercano <- function (long,lat){
+  
+  r2 <- r2 %>% mutate(distancia = distVincentyEllipsoid(c(long,lat),  r2[,c('w','n')])/1000) #en km
   
   head(r2[order(r2$distancia,decreasing = TRUE),],1) %>% select(w,n)
 
-
-  
 }
 
 ref <- refugio_cercano(150.234,23.450) #ejemplo
@@ -32,3 +33,14 @@ m <- leaflet() %>%
   addTiles() %>%  # Add default OpenStreetMap map tiles
   addMarkers(lng=-ref[[1]], lat=ref[[2]], popup="Refugio más cercano")
 m  # Print the map
+
+
+#####con municipio#####
+refugios_municipio <- function(municipio) {
+  coord <- r2 %>% filter(r2$municipio==municipio) %>% select(w,n) 
+  m <- leaflet() %>%
+    addTiles() %>%  # Add default OpenStreetMap map tiles
+    addMarkers(lng=-coord[[1]], lat=coord[[2]])
+  m  # Print the map
+  
+}
