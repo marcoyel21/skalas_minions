91039b2bf526e0a923921d0d542fcf0f2185c5c0
Marco Ramos <57338890+marcoyel21@users.noreply.github.com>
2021-10-15 12:47:24 -0500

    Se agrega script que reinstala paquetes

--- 1_packages.R
+++ 1_packages.R
@@ -1,14 +1,13 @@
-library(openxlsx)
-library(readxl)
-library(stringr) 
-library(stringi) 
-library(dplyr) 
-library(tidyverse)
-library(ggmap)
-library(geosphere)
-library(readr)
-library(leaflet)
-library(dplyr)
-library(purrr)
-library(DT)
-library(shiny)
+############################################################
+#### LIBRERIAS #############################################
+############################################################
+
+load.lib<-c("openxlsx", "readxl", "stringr",
+            "stringi","dplyr","ggmap",
+            "geosphere","leaflet","dplyr",
+            "purrr","DT","shiny")
+
+
+install.lib<-load.lib[!load.lib %in% installed.packages()]
+for(lib in install.lib) install.packages(lib,dependencies=TRUE)
+sapply(load.lib,require,character=TRUE)
